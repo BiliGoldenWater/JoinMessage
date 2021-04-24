23c422601336194f65f36d1fbe6a81142f70e880
BiliGoldenWater <2439577268@qq.com>
2021-04-25 03:18:44 +0800

    Initial commit

---
+++ src/main/java/indi/goldenwater/joinmessage/JoinMessage.java
@@ -0,0 +1,92 @@
+package indi.goldenwater.joinmessage;
+
+import org.bukkit.plugin.java.JavaPlugin;
+import org.bukkit.scheduler.BukkitRunnable;
+
+import java.io.IOException;
+import java.nio.file.*;
+
+public final class JoinMessage extends JavaPlugin {
+    private static JoinMessage instance;
+    private WatchService watchService;
+    private boolean watchServerRunning = false;
+    private boolean watchServiceRegistered = false;
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+        instance = this;
+        saveDefaultConfig();
+
+        if (getConfig().getBoolean("fileWatchService")) {
+            registerWatchService();
+            watchServiceRegistered = true;
+        }
+
+        getLogger().info("Enabled.");
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+        closeWatchService();
+
+        getLogger().info("Disabled.");
+    }
+
+    private void registerWatchService() {
+        try {
+            Path dataFolder = getDataFolder().toPath();
+            watchService = FileSystems.getDefault().newWatchService();
+            dataFolder.register(watchService, StandardWatchEventKinds.ENTRY_DELETE, StandardWatchEventKinds.ENTRY_MODIFY);
+            BukkitRunnable watchServiceRunnable = new BukkitRunnable() {
+
+                @Override
+                public void run() {
+                    try {
+                        WatchKey key;
+                        while (watchServerRunning && (key = watchService.take()) != null) {
+                            for (WatchEvent<?> event : key.pollEvents()) {
+                                if (event.kind().equals(StandardWatchEventKinds.ENTRY_DELETE)) {
+                                    if (event.context().toString().equals("config.yml")) {
+                                        saveDefaultConfig();
+                                    }
+                                } else if (event.kind().equals(StandardWatchEventKinds.ENTRY_MODIFY)) {
+                                    if (event.context().toString().equals("config.yml")) {
+                                        reloadConfig();
+                                        if (!getConfig().getBoolean("fileWatchService")) {
+                                            closeWatchService();
+                                        }
+                                    }
+                                }
+                            }
+                            key.reset();
+                        }
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                }
+            };
+            watchServerRunning = true;
+            watchServiceRunnable.runTaskAsynchronously(this);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void closeWatchService() {
+        if (watchServiceRegistered) {
+            try {
+                watchServerRunning = false;
+                watchService.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+        watchServiceRegistered = false;
+    }
+
+    public static JoinMessage getInstance() {
+        return instance;
+    }
+}
